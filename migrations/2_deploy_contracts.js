1107411b76f2c8668d1211e7b9f319115fac11b8
shiv4705 <shivpatel4705.sp@gmail.com>
2025-02-27 23:53:45 +0530

    4.Token Sale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var ShivToken = artifacts.require("./ShivToken.sol");
+var ShivTokenSale = artifacts.require("./ShivTokenSale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ShivToken, 1000000);
+  deployer.deploy(ShivToken, 1000000).then(function() {
+    //Token price is 0.001 Ether
+    var tokenPrice = 1000000000000000;
+    return deployer.deploy(ShivTokenSale, ShivToken.address, tokenPrice);
+  });
 };
